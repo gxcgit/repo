8b31141d7798c1b4e4f575175b5f4912d60eb986
gxc <gxc@qq.com>
2023-03-16 00:12:48 +0800

    你

--- src/main/java/com/example/demo/hello.java
+++ src/main/java/com/example/demo/hello.java
@@ -2,8 +2,7 @@ package com.example.demo;
 
 public class hello {
     public static void main(String[] args) {
-        
-        System.out.println("你好");
+                System.out.println("你好");
 
 
 
